41ac5f7f712526ea9aeb34008976fa99dabfbfb5
KT147 <kevin.tammm@gmail.com>
2025-09-09 15:56:05 +0300

    max property

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -25,8 +25,8 @@ export default function NewEventsSection() {
   // }, []);
 
   const { data, isPending, isError, error } = useQuery({
-    queryKey: ["events"],
-    queryFn: fetchEvents,
+    queryKey: ["events", { max: 3 }],
+    queryFn: ({signal, queryKey}) => fetchEvents({signal, ...queryKey[1]}),
     staleTime: 5000,   // millal request saadetakse, default on 0
     gcTime: 30000  // garbage collection, kui kauaks datat hoitakse, default 5min
   });
